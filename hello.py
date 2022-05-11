6474ea4dd0c9ba7873b0c6875cb4adfb916a6beb
franmgl <franmgl2001@gmail.com>
2022-05-11 12:18:43 -0500

    Changed

---
+++ hello.py
@@ -0,0 +1,12 @@
+"""
+Simple test program in python 
+
+Francisco Martinez Gallardo Lascurain
+"""
+
+def main():
+
+    print("Hello World")
+    for i in range(5):
+        print("New line")
+main()
