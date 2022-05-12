ddae2ad7b2a93a076c69e440603d14339fac9dae
Santiago Herrero <herrerosantiago2001@hotmail.com>
2022-05-12 10:32:58 -0500

    Addes triangle function

--- Python/paint.py
+++ Python/paint.py
@@ -20,6 +20,7 @@ def line(start, end):
     down()
     goto(end.x, end.y)
 
+
 def square(start, end):
     "Draw square from start to end."
     up()
@@ -33,18 +34,28 @@ def square(start, end):
 
     end_fill()
 
-def circle(start, end):
-    "Draw circle from start to end."
-    pass  # TODO
+def triangle(start, end):
+    "Draw triangle from start to end."
 
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x-start.x)
+        left(120)
+
+    end_fill()
 def rectangle(start, end):
     "Draw rectangle from start to end."
     pass  # TODO
 
-def triangle(start, end):
-    "Draw triangle from start to end."
+def circle(start, end):
+    "Draw circle from start to end."
     pass  # TODO
 
+    up()
 def tap(x, y):
     "Store starting point or draw shape."
     start = state['start']
